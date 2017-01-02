5e3c3fa75a267855451d1746f95c8bd3e16235bf
microbye <1090184016@qq.com>
2017-01-02 17:07:52 +0800

    the fifth commit

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var fs= require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var list = comment.getComment();
-  console.log(list);
-  res.render('index', { title: 'Hello world',list: list });
+  //var list = comment.getComment();
+  //console.log(list);
+  res.render('index', { title: 'Hello world'});
 });
 
 module.exports = router;
