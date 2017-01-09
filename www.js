0a59f1afc18008e8fdae295c68eba99356d189e3
microbye <1090184016@qq.com>
2017-01-09 22:11:58 +0800

    microbye

--- www.js
+++ www.js
@@ -4,5 +4,5 @@ http.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.write('<h1>Node.js</h1>');
 res.end('<p>Hello World</p>');
-}).listen(3000);
-console.log("HTTP server is listening at port 3000.");
+}).listen(1337);
+console.log("HTTP server is listening at port 1337.");
